2b5ae998ed0e968a2ea71447c5e65d72e6f2a908
UrgingGamer <90156029+UrgingGamer@users.noreply.github.com>
2021-09-07 18:53:03 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config               = {}
+Config.usecommand    = false
+Config.usekeybinding = true
+Config.jobname       = 'military'
+
+Config.modifierkey = 21 -- Left Shift
+Config.code1key = 157 -- 1
+Config.code2key = 158 -- 2
+Config.code3key = 160 --  3
+Config.code99key = 164 --  4
---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+----------------------------------------------------------
+--					      ug_militaryJob_backup					       	--
+--				     Script made by: UrigngGamer			       	--
+----------------------------------------------------------
+
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Backup Dependencie for Military Job (Made By: UrgingGamer)'
+
+version '1.0.0'
+
+client_scripts {
+  'config.lua',
+	'client.lua'
+}
+
+server_scripts {
+  'config.lua',
+	'server.lua'
+}
